a8d0c139ed48abfc48c221a63433b1f2d7f431fd
Cameron <1014Cameron@gmail.com>
2024-01-29 16:12:05 -0600

    search bar added to the landing page

---
+++ ghi/src/components/Home/CardComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CardComponent = () => {
+    return (
+        <div>
+            {/* Paste your HTML code here */}
+            <div class="your-card-class">
+                <!-- Your card content -->
+            </div>
+        </div>
+    );
+};
+
+export default CardComponent;
