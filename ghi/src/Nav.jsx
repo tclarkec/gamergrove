318fd591e98c149aae21fe515bba8df5d1f246a6
tclarkec1 <tclarkecarpenter3d@gmail.com>
2024-01-31 12:53:51 -0600

    Landing Page WIP

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -1,45 +1,76 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.css';
-import { getImageUrl } from './utlity';
-import styles from './Nav.css'
-import logo from './assets/logo.gif'
-
+import logo from './assets/logo.gif';
 
 const Nav = () => {
-    const [display, handleDisplay] = useState(false);
-    const transitionNavBar = () => {
-        if (window.scrollY > 100) {
-            handleDisplay(true);
-        } else {
-            handleDisplay(false)
-        }
+  const [display, handleDisplay] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const transitionNavBar = () => {
+    handleDisplay(window.scrollY > 100);
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown((prev) => !prev);
+  };
+
+  const closeDropdown = (e) => {
+  const avatarContainer = document.getElementById('avatar-container');
+  // Close dropdown if the click is outside the avatar container
+  if (avatarContainer && !avatarContainer.contains(e.target)) {
+    setShowDropdown(false);
+  }
+};
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavBar);
+    return () => window.removeEventListener('scroll', transitionNavBar);
+  }, []);
+
+  useEffect(() => {
+    if (showDropdown) {
+      window.addEventListener('click', closeDropdown);
+    } else {
+      window.removeEventListener('click', closeDropdown);
     }
 
-    useEffect(() => {
-        window.addEventListener("scroll", transitionNavBar);
-        return() => window.removeEventListener("scroll", transitionNavBar);
-    }, [])
+    return () => {
+      window.removeEventListener('click', closeDropdown);
+    };
+  }, [showDropdown]);
 
   return (
-  <div className={`nav ${display && 'nav__black'}`}>
-    <nav>
-      <div className='nav__contents'>
-        <div className="ncontainer expanded">
-          <input placeholder='Search...' className='js-search' type="text" />
-          <i className="fa fa-search"></i>
+    <div className={`nav ${display && 'nav__black'}`}>
+      <nav>
+        <div className='nav__contents'>
+          <div className='ncontainer expanded'>
+            <input placeholder='Search...' className='js-search' type='text' />
+            <i className='fa fa-search'></i>
+          </div>
+
+          <img className='nav__logo' src={logo} alt='' />
+
+          <div
+            id='avatar-container'
+            className='nav__avatar-container'
+            onClick={toggleDropdown}
+          >
+            <img
+              className='nav__avatar'
+              src='https://i.postimg.cc/SQCfRFsN/image-9.png'
+              alt=''
+            />
+            {showDropdown && (
+              <div className='nav__dropdown'>
+                <div className='nav__dropdown-item'>Login</div>
+                <div className='nav__dropdown-item'>Sign Up</div>
+              </div>
+            )}
+          </div>
         </div>
-
-        <img className='nav__logo' src={logo} alt='' />
-        <img
-          className="nav__avatar"
-          src="https://i.postimg.cc/SQCfRFsN/image-9.png"
-          alt=""
-        />
-      </div>
-    </nav>
-  </div>
-);
-
-}
+      </nav>
+    </div>
+  );
+};
 
 export default Nav;
--- ghi/src/components/Games/Listgames.jsx
+++ ghi/src/components/Games/Listgames.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Menu from '../Home/Menu.jsx';
 import Nav from '../../Nav.jsx';
+import HomeGameCard from '../Cards/homeGameCard.jsx';
 
 const Listgames = () => {
   return (
@@ -8,8 +9,43 @@ const Listgames = () => {
         <Menu />
         <Nav />
 
-        <main>
-        
+        <main className='lgamesbody'>
+
+
+            <h3 className='banana'>Games/Popular</h3>
+
+
+            <hr class='lgamessolid'/>
+            <div className='grow'>
+
+
+            <div class="line"></div>
+            <div className='grow__posters'>
+
+                <HomeGameCard />
+                <HomeGameCard />
+                <HomeGameCard />
+                <HomeGameCard />
+
+
+
+
+                </div>
+            </div>
+
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
 
 
         </main>
