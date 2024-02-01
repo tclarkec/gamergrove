487a80b85255f81b154810161f112fad02281be7
Austin Kim <mambafinisher@gmail.com>
2024-02-01 14:53:45 -0800

    Updated replies to exclude title column

--- api/migrations/008_replies_table.py
+++ api/migrations/008_replies_table.py
@@ -3,7 +3,6 @@ steps = [
         """
         CREATE TABLE replies (
         id SERIAL PRIMARY KEY,
-        title VARCHAR(255),
         body TEXT,
         account_id INT NOT NULL,
         FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE,
