b476519b3e64e9807ac8d746b35019113a250baf
Cameron <1014Cameron@gmail.com>
2024-02-01 07:43:22 -0600

    So the onClick is working but returns 400 request in console

--- ghi/src/components/Cards/homeGameCard.jsx
+++ ghi/src/components/Cards/homeGameCard.jsx
@@ -18,19 +18,16 @@ function HomeGameCard() {
     fetchData();
   }, []);
 
-  const openPurchaseLink = (platform) => {
-    const platformStore = gameDataList.find((game) => game[platform]);
-    if (platformStore) {
-      const storeUrl = fetchStoreUrl(platform);
-      if (storeUrl) {
-        window.location.href = storeUrl;
-      }
+  const handleClick = async (platform, rawg_pk) => {
+    const storeUrl = await fetchStoreUrl(platform, rawg_pk);
+    if (storeUrl) {
+      window.location.href = storeUrl;
     }
   };
 
-  const fetchStoreUrl = async (platform) => {
+  const fetchStoreUrl = async (platform, rawg_pk) => {
     try {
-      const response = await fetch(`http://localhost:8000/api/stores{rawg_pk}?platform=${platform}`);
+      const response = await fetch(`http://localhost:8000/api/stores/${rawg_pk}?platform=${platform}`);
       const data = await response.json();
       return data[0]?.url;
     } catch (error) {
@@ -57,28 +54,28 @@ function HomeGameCard() {
               width="15px"
               height="15px"
               alt="Icon 1"
-              onClick={() => openPurchaseLink('Xbox')}
+              onClick={() => handleClick('Xbox', gameData.rawg_pk)}
             />
             <img
               src="https://cdn.icon-icons.com/icons2/2429/PNG/512/playstation_logo_icon_147249.png"
               width="15px"
               height="15px"
               alt="Icon 2"
-              onClick={() => openPurchaseLink('PlayStation')}
+              onClick={() => handleClick('PlayStation', gameData.rawg_pk)}
             />
             <img
               src="https://i.postimg.cc/R0qXLppc/image-3.png"
               width="15px"
               height="15px"
               alt="Icon 3"
-              onClick={() => openPurchaseLink('Nintendo')}
+              onClick={() => handleClick('Nintendo', gameData.rawg_pk)}
             />
             <img
               src="https://imgtr.ee/images/2024/01/29/85a2afdfc48ffb6bf795b565eba3de63.png"
               width="15px"
               height="15px"
               alt="Icon 4"
-              onClick={() => openPurchaseLink('PC')}
+              onClick={() => handleClick('PC', gameData.rawg_pk)}
             />
           </div>
           <div className="hgcontent-body">
