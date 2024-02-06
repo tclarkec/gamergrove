1eaafe7998a5c03e038452841752ff4c07d29f7f
kyle <talondrvr38@gmail.com>
2024-02-06 15:32:05 -0600

    store icons fixed for search results page

--- ghi/src/components/SearchResults/SearchResults.jsx
+++ ghi/src/components/SearchResults/SearchResults.jsx
@@ -77,34 +77,45 @@ useEffect(() => {
                 </div>
             </Link>
             <div className="hgcontent-capsules">
-                <img
+            {gameData.xbox && (
+              <img
                 src="https://i.postimg.cc/nrDT7szB/image-5.png"
                 width="15px"
                 height="15px"
                 alt="Icon 1"
                 onClick={() => handleClick('Xbox', gameData.rawg_pk)}
-                />
+              />
+            )}
+
+            {gameData.playstation && (
                 <img
                 src="https://cdn.icon-icons.com/icons2/2429/PNG/512/playstation_logo_icon_147249.png"
                 width="15px"
                 height="15px"
                 alt="Icon 2"
                 onClick={() => handleClick('PlayStation', gameData.rawg_pk)}
-                />
-                <img
+              />
+            )}
+
+
+           {gameData.nintendo && (
+              <img
                 src="https://i.postimg.cc/R0qXLppc/image-3.png"
                 width="15px"
                 height="15px"
                 alt="Icon 3"
                 onClick={() => handleClick('Nintendo', gameData.rawg_pk)}
-                />
-                <img
+              />
+            )}
+            {gameData.pc && (
+              <img
                 src="https://imgtr.ee/images/2024/01/29/85a2afdfc48ffb6bf795b565eba3de63.png"
                 width="15px"
                 height="15px"
                 alt="Icon 4"
                 onClick={() => handleClick('PC', gameData.rawg_pk)}
-                />
+              />
+            )}
             </div>
             <div className="hgcontent-body">
                 <p>{parse(gameData.description.slice(0, 200))}</p>
