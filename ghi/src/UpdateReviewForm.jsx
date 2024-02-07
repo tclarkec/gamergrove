ffc2f2dfa98b41b8dac7abc1878b4133aaefe989
Austin Kim <mambafinisher@gmail.com>
2024-02-07 12:17:49 -0800

    Can update a review from games detail page

--- ghi/src/UpdateReviewForm.jsx
+++ ghi/src/UpdateReviewForm.jsx
@@ -2,55 +2,87 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import StarRating from './StarRating';
 
-async function fetchAccountId() {
+const fetchUserName = async () => {
   const tokenUrl = `http://localhost:8000/token`;
 
   const fetchConfig = {
     credentials: 'include',
   };
 
-  try {
-    const response = await fetch(tokenUrl, fetchConfig);
+  const response = await fetch(tokenUrl, fetchConfig);
 
-    if (response.ok) {
-      const data = await response.json();
-      return data.account.id;
-    } else {
-      console.error('Error fetching account ID:', response.status);
-      return null;
-    }
-  } catch (error) {
-    console.error('Error fetching account ID:', error);
-    return null;
+  if (response.ok) {
+    const data = await response.json();
+    return data.account.username;
+  }
+};
+
+const saved_username = await fetchUserName();
+
+const fetchAccount = async () => {
+  const accountUrl = `http://localhost:8000/api/accounts/${saved_username}`;
+
+  const response = await fetch(accountUrl);
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+};
+
+const account_data = await fetchAccount();
+
+async function fetchReviews(id) {
+  const reviewUrl = `http://localhost:8000/api/reviews/${id}`;
+  const reviewConfig = {
+    credentials: 'include'
+  };
+
+  const response = await fetch(reviewUrl, reviewConfig);
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
   }
 }
 
 function UpdateReviewForm() {
   const navigate = useNavigate();
   const { review_id, game_id } = useParams();
-  const initialData = {
-    title: "",
-    body: "",
-    game_id: game_id,
-    rating: ""
-  };
-
-  const [formData, setFormData] = useState(initialData);
-  const [accountId, setAccountId] = useState(null);
+  const [formData, setFormData] = useState({
+    title: '',
+    body: '',
+    game_id: `${game_id}`,
+    rating: ''
+  });
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      const fetchedAccountId = await fetchAccountId();
-      if (fetchedAccountId) {
-        setAccountId(fetchedAccountId);
-        console.log('Got account id!');
-      } else {
-        console.error('Error fetching account ID');
+    const fetchData = async () => {
+      try {
+        const reviewData = await fetchReviews(review_id);
+
+        if (reviewData) {
+          setFormData({
+            title: reviewData.title || '',
+            body: reviewData.body || '',
+            game_id: reviewData.game_id,
+            rating: reviewData.rating || ''
+          });
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
     };
 
-    fetchUserData();
-  }, []);
+    fetchData();
+  }, [review_id]);
+
+    const handleStarClick = (selectedRating) => {
+    setFormData({
+      ...formData,
+      rating: selectedRating,
+    });
+  };
 
   const handleFormChange = (e) => {
     setFormData({
@@ -59,20 +91,13 @@ function UpdateReviewForm() {
     });
   };
 
-  const handleStarClick = (selectedRating) => {
-    setFormData({
-      ...formData,
-      rating: selectedRating,
-    });
-  };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const reviewUrl = `http://localhost:8000/api/reviews/users/${review_id}/${accountId}`;
+    const reviewsUrl = `http://localhost:8000/api/reviews/${review_id}/${account_data.id}`;
 
     const fetchConfig = {
-      method: "put",
+      method: 'put',
       body: JSON.stringify(formData),
       credentials: 'include',
       headers: {
@@ -80,16 +105,12 @@ function UpdateReviewForm() {
       }
     };
 
-    try {
-      const response = await fetch(reviewUrl, fetchConfig);
-      if (response.ok) {
-        navigate("/dashboard");
-        setFormData(initialData);
-      } else {
-        throw new Error('Failed to update review');
-      }
-    } catch (error) {
-      console.error('Error updating review:', error);
+    const response = await fetch(reviewsUrl, fetchConfig);
+
+    if (response.ok) {
+      navigate(`/games/${game_id}`);
+    } else {
+      console.error('Failed to update review');
     }
   };
 
@@ -98,18 +119,18 @@ function UpdateReviewForm() {
       <div className="row">
         <div className="offset-3 col-6">
           <div className="shadow p-4 mt-4">
-            <h1>Update a review</h1>
+            <h1>Update this review</h1>
             <form onSubmit={handleSubmit} id="create-board">
               <div className="form-floating mb-3">
                 <label htmlFor="title">Title</label>
-                <input onChange={handleFormChange} placeholder="i.e. The Best Game of 2023" required type="text" name="title" id="title" className="form-control" value={formData.title} />
+                <input onChange={handleFormChange} required type="text" name="title" id="title" className="form-control" value={formData.title} />
               </div>
               <div className="form-floating mb-3">
-                <label htmlFor="body">Body</label>
-                <textarea onChange={handleFormChange} placeholder="i.e. A groundbreaking FPS" name="body" id="body" className="form-control" value={formData.body} rows="3"></textarea>
+                <label htmlFor="body">Description</label>
+                <textarea onChange={handleFormChange} required name="body" id="body" className="form-control" value={formData.body} rows="3"></textarea>
               </div>
               <div className="form-floating mb-3">
-                <label htmlFor='rating' style={{ marginBottom: '0rem' }}>Give a rating out of 5:</label>
+                <label htmlFor='rating' style={{ marginBottom: '0rem' }}>Rating out of 5:</label>
                 <div className='rating-container d-flex justify-content-center'>
                   <div className='star-rating'>
                     <StarRating rating={formData.rating} onStarClick={handleStarClick} />
--- ghi/src/blank.jsx
+++ ghi/src/blank.jsx
@@ -1,116 +1,128 @@
-import {useAuthContext} from "@galvanize-inc/jwtdown-for-react";
-import useToken from "@galvanize-inc/jwtdown-for-react";
-import { useState, useEffect } from "react";
-import {useNavigate} from 'react-router-dom';
-import { useRef } from "react";
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import StarRating from './StarRating';
 
+async function fetchAccountId() {
+  const tokenUrl = `http://localhost:8000/token`;
 
-const containerStyle = {
-  minHeight: '100vh',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-};
-
-const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginSubmitted, setLoginSubmitted] = useState(false);
-  const [incorrectLogin, setIncorrectLogin] = useState(false);
-  const { login } = useToken();
-  const { token } = useAuthContext();
-  const navigate = useNavigate();
-
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoginSubmitted(true);
-    login(username, password)
-
-
+  const fetchConfig = {
+    credentials: 'include',
   };
 
-  console.log(loginSubmitted);
+  try {
+    const response = await fetch(tokenUrl, fetchConfig);
+
+    if (response.ok) {
+      const data = await response.json();
+      return data.account.id;
+    } else {
+      console.error('Error fetching account ID:', response.status);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching account ID:', error);
+    return null;
+  }
+}
+
+function UpdateReviewForm() {
+  const navigate = useNavigate();
+  const { review_id, game_id } = useParams();
+  const initialData = {
+    title: "",
+    body: "",
+    game_id: game_id,
+    rating: ""
+  };
+
+  const [formData, setFormData] = useState(initialData);
+  const [accountId, setAccountId] = useState(null);
 
   useEffect(() => {
-    const fetchToken = async (e) => {
-      const tokenUrl = `http://localhost:8000/token`;
-      const fetchConfig = {
-        credentials: 'include'
-      };
-
-      const response = await fetch(tokenUrl, fetchConfig);
-      console.log(response);
-      if (response.ok) {
-          navigate('/login/welcomeback');
+    const fetchUserData = async () => {
+      const fetchedAccountId = await fetchAccountId();
+      if (fetchedAccountId) {
+        setAccountId(fetchedAccountId);
+        console.log('Got account id!');
       } else {
-        setIncorrectLogin(true);
+        console.error('Error fetching account ID');
       }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const handleFormChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleStarClick = (selectedRating) => {
+    setFormData({
+      ...formData,
+      rating: selectedRating,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const reviewUrl = `http://localhost:8000/api/reviews/users/${review_id}/${accountId}`;
+
+    const fetchConfig = {
+      method: "put",
+      body: JSON.stringify(formData),
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const response = await fetch(reviewUrl, fetchConfig);
+      if (response.ok) {
+        navigate("/dashboard");
+        setFormData(initialData);
+      } else {
+        throw new Error('Failed to update review');
+      }
+    } catch (error) {
+      console.error('Error updating review:', error);
     }
-    if (loginSubmitted){
-      fetchToken()
-      }
-
-  },
-  [ loginSubmitted, token])
-
-  let messageClasses = 'alert alert-danger d-none mb-0';
-  let formClasses = '';
-  if (incorrectLogin) {
-    messageClasses = 'alert alert-danger mb-0';
-    formClasses = 'd-none';
-  }
+  };
 
   return (
-    <div style={{ position: 'relative' }}>
-      <button
-        onClick={() => { navigate("/"); }}
-        style={{
-          position: 'absolute',
-          top: '0',
-          left: '0',
-          margin: '10px',
-        }}
-      >
-        Back to Homepage
-      </button>
-    <div style={containerStyle}>
-    <div className="card text-bg-light mb-3">
-      <div className="offset-3 col-6">
-      <h5 className="card-header">Login</h5>
-      <div className="card-body">
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <div className="mb-3">
-            <label className="form-label">Username:</label>
-            <input
-              name="username"
-              type="text"
-              className="form-control"
-              onChange={(e) => setUsername(e.target.value)}
-            />
+    <div className="container">
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="shadow p-4 mt-4">
+            <h1>Update a review</h1>
+            <form onSubmit={handleSubmit} id="create-board">
+              <div className="form-floating mb-3">
+                <label htmlFor="title">Title</label>
+                <input onChange={handleFormChange} placeholder="i.e. The Best Game of 2023" required type="text" name="title" id="title" className="form-control" value={formData.title} />
+              </div>
+              <div className="form-floating mb-3">
+                <label htmlFor="body">Body</label>
+                <textarea onChange={handleFormChange} placeholder="i.e. A groundbreaking FPS" name="body" id="body" className="form-control" value={formData.body} rows="3"></textarea>
+              </div>
+              <div className="form-floating mb-3">
+                <label htmlFor='rating' style={{ marginBottom: '0rem' }}>Give a rating out of 5:</label>
+                <div className='rating-container d-flex justify-content-center'>
+                  <div className='star-rating'>
+                    <StarRating rating={formData.rating} onStarClick={handleStarClick} />
+                  </div>
+                </div>
+              </div>
+              <button>Update</button>
+            </form>
           </div>
-          <div className="mb-3">
-            <label className="form-label">Password:</label>
-            <input
-              name="password"
-              type="password"
-              className="form-control"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div>
-            <input style={{ marginBottom: '15px' }} type="submit" value="Login" />
-          </div>
-          <div className={messageClasses} id="failure-message">
-            Incorrect username or password...
-          </div>
-        </form>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
-  </div>
   );
-};
+}
 
-export default LoginForm;
+export default UpdateReviewForm;
