073c01f63be27f13700c1d56bc599e0f5aa203f8
Cameron <1014Cameron@gmail.com>
2024-02-09 09:18:11 -0600

    Verified my test function is working and updated my journals

---
+++ api/tests/test_stores.py
@@ -0,0 +1,30 @@
+from fastapi.testclient import TestClient
+from main import app
+from queries.stores import StoresOut, StoresQueries
+
+
+client = TestClient(app)
+
+
+# Arrange
+class MockStoresQueries:
+    def get_stores(self, rawg_pk: int):
+        return [
+            StoresOut(id=1, url="blueberryfin.co", platform='xbox', rawg_pk=rawg_pk),
+            StoresOut(id=2, url="bananaberryfin.co", platform="playstation", rawg_pk=rawg_pk)
+        ]
+
+
+def test_get_stores():
+    # Arrange
+    app.dependency_overrides[StoresQueries] = MockStoresQueries
+    rawg_pk = 423
+    # Act
+    response = client.get(f'/api/stores/{rawg_pk}')
+    # Assert
+    assert response.status_code == 200
+    stores = response.json()
+    assert len(stores) == 2
+    # assert all(isinstance(store, dict) for store in stores)
+    # Clean Up
+    app.dependency_overrides = {}
